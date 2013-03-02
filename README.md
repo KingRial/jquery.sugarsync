aa82b5c0681d872046b7b7d28e0a2f0a6c47c1a7
KingRial <fake@gmail.com>
2013-03-02 10:14:35 +0100

    First Commit
    This is the commit for release 0.0.1

---
+++ README.md
@@ -0,0 +1,134 @@
+jQuery SugarSync v0.0.1
+=================
+"jQuery SugarSync" is a jQuery Plugin to add SugarSync functionality to the jQuery UI (https://www.sugarsync.com/).
+
+License
+-------
+Copyright 2012, Riccardo Re
+
+Dual licensed under the MIT or GPL Version 2 licenses.
+
+<http://jquery.org/license>
+
+Usage
+-----
+```javascript
+$(selector).sugarsync([options]);
+```
+
++ `selector`
+    The jQuery selector, targeting an element you'd like to use a sugarsync plugin reference
++ `options`
+    An optional JavaScript object that you may pass if you would like to customize the experience with the plugin. Below is a list of properties that you may set on the options object and their respective effect.
+    * `options.username`
+        - The username to use for the sugarsync service
+    * `options.password`
+        - The password to use for the sugarsync service
+	* `options.applicationID` `{String: "using jQuery SugarSync's plugin"}`
+        - A sugarsync's application ID ot use a custom application
+    * `options.accessKeyID` `{String: "using jQuery SugarSync's plugin"}`
+        - A sugarsync's Access Key ID to use a custom application
+    * `options.privateAccessKey` `{String: "using jQuery SugarSync's plugin"}`
+        - A sugarsync's private access key to use a custom application
+
+```javascript
+$(selector).sugarsync('getUserInfo',fCallback);
+```
+
++ `selector`
+    The jQuery selector, targeting an element you are using as sugarsync plugin reference
++ `fCallback`
+    The callback function receiving a JavaScript object describing the user info as first parameter. Below is a list of properties which describe the user info.
+	* `username`
+        - The username
+	* `nickname`
+        - The nickname
+	* `quota.limit`
+        - The limit quota
+	* `quota.usage`
+        - The usage quota
+	* `workspaces`
+        - The workspace resource
+	* `syncfolders`
+        - The syncfolders resource
+	* `deleted`
+		- The deleted resource
+	* `magicBriefcase`
+		- The magicBriefcase resource
+	* `webArchive`
+		- The webArchive resource
+	* `mobilePhotos`
+		- The mobilePhotos resource
+	* `recentActivities`
+		- The recentActivities resource
+	* `receivedShares`
+		- The receivedShares resource
+	* `publicLinks`
+		- The publicLinks resource
+	* `maximumPublicLinkSize`
+		- The maximumPublicLinkSize number
+
+```javascript
+$(selector).sugarsync('getFolderInfo', sFolderResource, fCallback);
+```
+
++ `selector`
+    The jQuery selector, targeting an element you are using as sugarsync plugin reference
++ `sFolderResource`
+    The string describing a folder resource
++ `fCallback`
+    The callback function receiving a JavaScript object describing the folder info as first parameter. Below is a list of properties which describe the folder info.
+	* `displayName`
+		- The folder label
+	* `dsid`
+		- The folder dsid
+	* `timeCreated`
+		- The folder creation time
+	* `collections`
+		- The collections resource
+	* `files`
+		- The files resource
+	* `contents`
+		- The contents resource
+
+```javascript
+$(selector).sugarsync('getFolderList', sFolderResource, fCallback);
+```
+
++ `selector`
+    The jQuery selector, targeting an element you are using as sugarsync plugin reference
++ `sFolderResource`
+    The string describing a folder resource
++ `fCallback`
+    The callback function receiving a JavaScript object describing the folder contents as first parameter. Below is a list of properties which describe the folder contents.
+	* `dirList`
+		- An array of directories inside the resource; each element of the array is a JavaScript object with the following parameters:
+		`displayName` the label
+		`ref` the folder resource
+		
+	* `fileList`
+		- An array of files inside the resource; each element of the array is a JavaScript object with the following parameters:
+		`displayName` the label
+		`ref` the file resource
+		`size` the file size
+		`lastModified` the last time the file was modified
+		`mediaType` the file media type
+		`presentOnServer` a flag which shows if the file is present on the server
+		`fileData` the file data resource
+
+Notes
+-----
+
+Improvements
+-----
+A lot of enhancement can be done on this library.
+Right now I am planning to add as soon as possible:
+- Adding new options to getListContents function
+- File Creation
+- Upload Data operation to created file
+- Download file operation
+
+Change Log
+----------
+ * __0.0.1__
+  - Commited on Git Hub first release
---
+++ jquery.sugarsync.js
@@ -0,0 +1,390 @@
+/*!
+ * jQuery SugarSync 0.0.1
+ * http://
+ * https://www.sugarsync.com/
+ * https://www.sugarsync.com/developer
+ *
+ * Copyright 2012 Riccardo Re
+ * Released under the MIT license.
+ * http://jquery.org/license
+ *
+ * http://api
+ *
+ * Depends:
+ *	jquery.js
+ */
+ 
+(function( $ ){
+	var _XMLparser=function(xml){
+		var tmp=xml;
+		return new function(tmp){
+			try{
+				this._oXml=$((typeof xml=='string' && $.isXMLDoc(xml)?$.parseXML(xml):xml));
+			}catch(e){
+				this._oXml=null;
+			}
+			this.get=function(value,bUseLeafs){
+				var result=null;
+				if( this._oXml ){
+					var _aItem=this._oXml.find(value);
+					var _aItemChildren=_aItem.children();
+					result=[];
+					if( _aItem.length > 1 ){
+						for( var i=0;i<_aItem.length;i++){
+							result.push( this.get(value + ':eq(' + i + ') ',true) );
+						}
+					} else if( _aItemChildren.length > 0 ) {
+						var _oElement={};
+						for( var i=0;i<_aItemChildren.length;i++){
+							var _sTagName=_aItemChildren.eq(i).prop('tagName');
+							_oElement[_sTagName]=this.get(value + ' ' + _sTagName);
+						}
+						if( bUseLeafs ){
+							result=_oElement;
+						} else {
+							result.push( _oElement );
+						}
+					} else {
+						result = _aItem.text();
+					}
+				}
+				return ((isNaN(result)||result=='')?result:parseFloat(result));
+			}
+		}
+	}
+	
+	var _oMethods = {
+	//Init
+		init : function( options ) {
+			//Saving Options
+			var _oSettings = $.extend( {
+				username: null,
+				password: null,
+				accessKeyID: 'NTY2NDA4NDEzNTUzMDQxODU3MDY',
+				applicationID: '/sc/5664084/491_43083142',
+				privateAccessKey: 'MjE5OGMyMmVlNjAxNDZmNmJkZTM1MTY2OTE2N2UyZWI',
+				refreshToken: null,
+				accessToken: null,
+				accessTokenExpiration: null,
+				userResource: null
+			}, options );
+
+			return this.each(function(){
+				var $this = $(this),
+					data = $this.data('_sugarsync');
+				if ( !data ){
+					$(this).data('_sugarsync', _oSettings);
+				}
+			});
+		},
+	//Request Refresh Token
+		requestRefreshToken : function(fCallback){
+			var $this = $(this),
+				_oSettings = $this.data('_sugarsync');
+			var _sXml = '<?xml version="1.0" encoding="UTF-8"?><appAuthorization><username>'+_oSettings.username+'</username><password>'+_oSettings.password+'</password><application>'+_oSettings.applicationID+'</application><accessKeyId>'+_oSettings.accessKeyID+'</accessKeyId><privateAccessKey>'+_oSettings.privateAccessKey+'</privateAccessKey></appAuthorization>';
+			if( !_oSettings.refreshToken ){
+				$.ajax({
+					url: 'https://api.sugarsync.com/app-authorization',
+					type: 'POST',
+					processData: false,
+					data: $.parseXML(_sXml), 
+					contentType: 'application/xml; charset=UTF-8',
+					success:$.proxy($this.sugarsync,$this,'_requestRefreshTokenSuccess',fCallback),
+					error:$.proxy($this.sugarsync,$this,'_requestRefreshTokenError',fCallback)
+				});
+			} else if( fCallback ){
+				fCallback();
+			}
+		},
+		_requestRefreshTokenSuccess : function(fCallback, data, textStatus, jqXHR){
+			var $this = $(this),
+				_oSettings = $this.data('_sugarsync');
+			_oSettings.refreshToken=jqXHR.getResponseHeader('Location');
+			$this.data('_sugarsync',_oSettings);
+			if( fCallback ){
+				fCallback();
+			}
+		},
+		_requestRefreshTokenError : function(fCallback, jqXHR, textStatus, errorThrown){
+			$.error( 'Unable to get REFRESH token: '+textStatus+' - '+errorThrown);
+			if( fCallback ){
+				fCallback();
+			}
+		},
+	//Request Access Token
+		requestAccessToken : function(fCallback){
+			var $this = $(this),
+				_oSettings = $this.data('_sugarsync');
+			if( !_oSettings.accessToken || ( (new Date().getTime()/1000) >= _oSettings.accessTokenExpiration ) ){
+				var _sXml = '<?xml version="1.0" encoding="UTF-8"?><tokenAuthRequest><accessKeyId>'+_oSettings.accessKeyID+'</accessKeyId><privateAccessKey>'+_oSettings.privateAccessKey+'</privateAccessKey><refreshToken>'+_oSettings.refreshToken+'</refreshToken></tokenAuthRequest>';
+				$.ajax({
+					url: 'https://api.sugarsync.com/authorization',
+					type: 'POST',
+					processData: false,
+					data: $.parseXML(_sXml), 
+					contentType: 'application/xml; charset=UTF-8',
+					success:$.proxy($this.sugarsync,$this,'_requestAccessTokenSuccess',fCallback),
+					error:$.proxy($this.sugarsync,$this,'_requestAccessTokenError',fCallback)
+				});
+			} else if( fCallback ){
+				fCallback();
+			}
+		},
+		_requestAccessTokenSuccess : function(fCallback, data, textStatus, jqXHR){
+			var $this = $(this),
+				_oSettings = $this.data('_sugarsync');
+			_oSettings.accessToken=jqXHR.getResponseHeader('Location');
+			var _oXMLresponse=_XMLparser(data);
+			_oSettings.accessTokenExpiration=Date.parse(_oXMLresponse.get('expiration'))/1000;
+			_oSettings.userResource=_oXMLresponse.get('user');
+			$this.data('_sugarsync',_oSettings);
+			if( fCallback ){
+				fCallback();
+			}
+		},
+		_requestAccessTokenError : function(fCallback, jqXHR, textStatus, errorThrown){
+			$.error( 'Unable to get ACCESS token: '+textStatus+' - '+errorThrown);
+			if( fCallback ){
+				fCallback();
+			}
+		},
+	//Get User Info
+		getUserInfo : function(fCallback){
+			return this.each(function(){
+				var $this = $(this);
+				$this.sugarsync('requestRefreshToken',
+					$.proxy($this.sugarsync,$this,'requestAccessToken',
+						$.proxy($this.sugarsync,$this,'_getUserInfoRequest',
+							fCallback
+						)
+					)
+				);
+			})
+		},
+		_getUserInfoRequest : function(fCallback){
+			var $this = $(this),
+				_oSettings = $this.data('_sugarsync');
+			if( _oSettings.userResource ){
+				var _sXml = '<?xml version="1.0" encoding="UTF-8"?>';
+				$.ajax({
+					url: _oSettings.userResource,
+					type: 'GET',
+					headers:{
+						Authorization:_oSettings.accessToken
+					},
+					processData: false,
+					contentType: 'application/xml; charset=UTF-8',
+					success:$.proxy($this.sugarsync,$this,'_getUserInfoRequestSuccess',fCallback),
+					error:$.proxy($this.sugarsync,$this,'_getUserInfoRequestError',fCallback)
+				});
+			} else if( fCallback ){
+				$.error( 'Cannot find a user resource; something went wrong.' );
+				fCallback();
+			}
+		},
+		_getUserInfoRequestSuccess : function(fCallback, data, textStatus, jqXHR){
+			var _oXMLresponse=_XMLparser(data);
+			var _oResponse={
+				username:_oXMLresponse.get('username'),
+				nickname:_oXMLresponse.get('nickname'),
+				quota:{
+					limit:_oXMLresponse.get('quota limit'),
+					usage:_oXMLresponse.get('quota usage'),
+				},
+				workspaces:_oXMLresponse.get('workspaces'),
+				syncfolders:_oXMLresponse.get('syncfolders'),
+				deleted:_oXMLresponse.get('deleted'),
+				magicBriefcase:_oXMLresponse.get('magicBriefcase'),
+				webArchive:_oXMLresponse.get('webArchive'),
+				mobilePhotos:_oXMLresponse.get('mobilePhotos'),
+				albums:_oXMLresponse.get('albums'),
+				recentActivities:_oXMLresponse.get('recentActivities'),
+				receivedShares:_oXMLresponse.get('receivedShares'),
+				publicLinks:_oXMLresponse.get('publicLinks'),
+				maximumPublicLinkSize:_oXMLresponse.get('maximumPublicLinkSize')
+			};
+			if( fCallback ){
+				fCallback(_oResponse);
+			}
+		},
+		_getUserInfoRequestError : function(fCallback, jqXHR, textStatus, errorThrown){
+			$.error( 'Unable to get USER INFOs: '+textStatus+' - '+errorThrown);
+			if( fCallback ){
+				fCallback();
+			}
+		},
+	//Get Folder Info
+		getFolderInfo : function(sFolderResource,fCallback,oOptions){
+			return this.each(function(){
+				var $this = $(this);
+				$this.sugarsync('requestRefreshToken',
+					$.proxy($this.sugarsync,$this,'requestAccessToken',
+						$.proxy($this.sugarsync,$this,'_getFolder',
+							sFolderResource,
+							fCallback,
+							{type:'info'}
+						)
+					)
+				);
+			})
+		},
+	//Get Folder Contents
+		getFolderContents : function(sFolderResource,fCallback,oOptions){
+			return this.each(function(){
+				var $this = $(this);
+				$this.sugarsync('requestRefreshToken',
+					$.proxy($this.sugarsync,$this,'requestAccessToken',
+						$.proxy($this.sugarsync,$this,'_getFolder',
+							sFolderResource,
+							fCallback,
+							{type:'contents'}
+						)
+					)
+				);
+			})
+		},
+	//Get Folder
+		_getFolder : function(sFolderResource,fCallback,oOptions){
+			return this.each(function(){
+				var $this = $(this);
+				$this.sugarsync('requestRefreshToken',
+					$.proxy($this.sugarsync,$this,'requestAccessToken',
+						$.proxy($this.sugarsync,$this,'_getFolderRequest',
+							sFolderResource,
+							fCallback,
+							oOptions
+						)
+					)
+				);
+			})
+		},
+		_getFolderRequest : function(sFolderResource,fCallback,oOptions){
+			if(!oOptions)oOptions={};
+			if(!oOptions.type)oOptions.type='info';//info or content
+			var $this = $(this),
+				_oSettings = $this.data('_sugarsync');
+			if( _oSettings.userResource ){
+				var _sXml = '<?xml version="1.0" encoding="UTF-8"?>';
+				var _sUrl=sFolderResource + ( oOptions.type=='contents' ? '/contents':'' );
+				$.ajax({
+					url: _sUrl,
+					type: 'GET',
+					headers:{
+						Authorization:_oSettings.accessToken
+					},
+					processData: false,
+					contentType: 'application/xml; charset=UTF-8',
+					success:$.proxy($this.sugarsync,$this,'_getFolderRequestSuccess',oOptions,fCallback),
+					error:$.proxy($this.sugarsync,$this,'_getFolderRequestError',oOptions,fCallback)
+				});
+			} else if( fCallback ){
+				$.error( 'Cannot find a user resource; something went wrong.' );
+				fCallback();
+			}
+		},
+		_getFolderRequestSuccess : function(oOptions,fCallback, data, textStatus, jqXHR){
+			var _oXMLresponse=_XMLparser(data);
+			var _oResponse={};
+			//Type: info
+			switch(oOptions.type){
+				case 'info':
+					_oResponse={
+						displayName:_oXMLresponse.get('displayName'),
+						dsid:_oXMLresponse.get('dsid'),
+						timeCreated:_oXMLresponse.get('timeCreated'),
+						collections:_oXMLresponse.get('collections'),
+						files:_oXMLresponse.get('files'),
+						contents:_oXMLresponse.get('contents')
+					};
+				break;
+				case 'contents':
+				default:
+					var _aDirList = _oXMLresponse.get('collection');
+					var _aFileList = _oXMLresponse.get('file');
+					_oResponse={
+						dirList:_aDirList,
+						fileList:_aFileList
+					};
+				break;
+			}
+			if( fCallback ){
+				fCallback(_oResponse);
+			}
+		},
+		_getFolderRequestError : function(oOptions,fCallback, jqXHR, textStatus, errorThrown){
+			$.error( 'Unable to get FOLDER INFOs: '+textStatus+' - '+errorThrown);
+			if( fCallback ){
+				fCallback();
+			}
+		},
+	//download (Needs server side help to download a file)
+/*
+		download : function(sFileResource,fCallback,oOptions){
+			return this.each(function(){
+				var $this = $(this);
+				$this.sugarsync('requestRefreshToken',
+					$.proxy($this.sugarsync,$this,'requestAccessToken',
+						$.proxy($this.sugarsync,$this,'_downloadRequest',
+							sFileResource,
+							fCallback
+						)
+					)
+				);
+			})
+		},
+		_downloadRequest : function(sFileResource,fCallback,oOptions){
+			if(!oOptions)oOptions={};
+			if(!oOptions.type)oOptions.type='info';//info or content
+			var $this = $(this),
+				_oSettings = $this.data('_sugarsync');
+			if( _oSettings.userResource ){
+				var _sXml = '<?xml version="1.0" encoding="UTF-8"?>';
+				var _sUrl=sFileResource;
+				$.ajax({
+					url: _sUrl,
+					type: 'GET',
+					headers:{
+						Authorization:_oSettings.accessToken
+					},
+					processData: false,
+					contentType: 'application/xml; charset=UTF-8',
+					success:$.proxy($this.sugarsync,$this,'_downloadRequestSuccess',oOptions,fCallback),
+					error:$.proxy($this.sugarsync,$this,'_downloadRequestError',oOptions,fCallback)
+				});
+			} else if( fCallback ){
+				$.error( 'Cannot find a user resource; something went wrong.' );
+				fCallback();
+			}
+		},
+		_downloadRequestSuccess : function(oOptions,fCallback, data, textStatus, jqXHR){
+			if( fCallback ){
+				fCallback();
+			}
+		},
+		_downloadRequestError : function(oOptions,fCallback, jqXHR, textStatus, errorThrown){
+			$.error( 'Unable to DOWNLOAD file: '+textStatus+' - '+errorThrown);
+			if( fCallback ){
+				fCallback();
+			}
+		},
+*/
+	//Destroy
+		destroy : function( ){
+			return this.each(function(){
+				var $this = $(this),
+					data = $this.data('_sugarsync');
+				data.sugarsync.remove();
+				$this.removeData('_sugarsync');
+			})
+		}
+	};
+	$.fn.sugarsync = function( method ) {
+		if ( _oMethods[method] ) {
+			return _oMethods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return _oMethods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.sugarsync' );
+		}
+	};
+})( jQuery );
